68037478b1b9c2f9000c5e8f44ce886d4adaa91b
aurelienlefrere <aurelienlefrere@gmail.com>
2025-11-19 12:59:12 +0100

    yelp_mvp_partie_1_spec_01_check

---
+++ app/models/restaurant.rb
@@ -0,0 +1,11 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  CATEGORY = %w(chinese italian japanese french belgian)
+
+  validates :name, presence: true
+  validates :category, inclusion: {in: CATEGORY}
+  validates :address, presence: true
+
+
+end
